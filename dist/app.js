c270063ff724dfbaf90dbc600be418ed9f9f8209
DeboraLedo <ledo.debora@gmail.com>
2022-10-20 20:22:47 -0300

    casi terminado

--- dist/app.js
+++ dist/app.js
@@ -4,13 +4,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const express_handlebars_1 = require("express-handlebars");
+const path_1 = __importDefault(require("path"));
+//routes
+const index_1 = __importDefault(require("./routes/index"));
+const tasks_1 = __importDefault(require("./routes/tasks"));
 class Aplication {
     constructor() {
         this.app = (0, express_1.default)();
+        this.settings();
+        this.middlewares();
+        this.routes();
+    }
+    settings() {
+        this.app.set("port", 3000);
+        this.app.set("views", path_1.default.join(__dirname, "views"));
+        this.app.engine(".hbs", (0, express_handlebars_1.create)({
+            layoutsDir: path_1.default.join(this.app.get("views"), "layouts"),
+            partialsDir: path_1.default.join(this.app.get("views"), "partials"),
+            defaultLayout: "main",
+            extname: ".hbs",
+        }).engine);
+        this.app.set("view engine", ".hbs");
+    }
+    middlewares() {
+        this.app.use((0, morgan_1.default)("dev"));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
+    }
+    routes() {
+        this.app.use(index_1.default);
+        this.app.use("/tasks", tasks_1.default);
+        this.app.use(express_1.default.static(path_1.default.join(__dirname, "/public"))); //no funciona
     }
     start() {
-        this.app.listen(3000, () => {
-            console.log("Server running");
+        this.app.listen(this.app.get("port"), () => {
+            console.log("Server on port", this.app.get("port"));
         });
     }
 }
